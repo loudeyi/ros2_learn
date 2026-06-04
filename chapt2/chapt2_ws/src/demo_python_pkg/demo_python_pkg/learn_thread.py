0a60bc869511a8a1e42d4adc6f4ecdf65a23f475
loudeyi <1160965856@qq.com>
2026-06-04 21:16:30 +0800

    6.4新添加与更改

--- chapt2/chapt2_ws/src/demo_python_pkg/demo_python_pkg/learn_thread.py
+++ chapt2/chapt2_ws/src/demo_python_pkg/demo_python_pkg/learn_thread.py
@@ -1,7 +1,7 @@
 import threading
 import requests
 
-class Donwload:
+class Download:
     def download(self, url, callback_word_count):
         print(f'线程:{threading.get_ident()} 开始下载:{url}')
         # 模拟下载过程
@@ -9,7 +9,7 @@ class Donwload:
         response.encoding = 'utf-8'
         callback_word_count(url,response.text)
 
-    def start_donwload(self, url, callback_word_count):
+    def start_download(self, url, callback_word_count):
         thread = threading.Thread(target=self.donload, args=(url, callback_word_count))
         thread.start()
 
@@ -22,7 +22,7 @@ def word_count(url, result):
 
 
 def main():
-    download = Donwload()
-    download.start_donwload('http://0.0.0.0:8000/novel1.txt', word_count)
-    download.start_donwload('http://0.0.0.0:8000/novel2.txt', word_count)
-    download.start_donwload('http://0.0.0.0:8000/novel3.txt', word_count)
+    download = Download()
+    download.start_download('http://0.0.0.0:8000/novel1.txt', word_count)
+    download.start_download('http://0.0.0.0:8000/novel2.txt', word_count)
+    download.start_download('http://0.0.0.0:8000/novel3.txt', word_count)
