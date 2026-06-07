9407db5564bfde2736ef393ebe1b2b8b2d86d4f0
loudeyi <1160965856@qq.com>
2026-06-07 21:57:35 +0800

    6.7 face_detect_client_node

--- chapt4/chapt4_ws/src/demo_python_service/setup.py
+++ chapt4/chapt4_ws/src/demo_python_service/setup.py
@@ -27,6 +27,7 @@ setup(
         'console_scripts': [
             'learn_face_detect=demo_python_service.learn_face_detect:main',
             'face_detect_node=demo_python_service.face_detect_node:main',
+            'face_detect_client_node=demo_python_service.face_detect_client_node:main',
         ],
     },
 )
