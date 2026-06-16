7e0a18a32dae5bbb79f1c7800e108eb3bb62f036
loudeyi <1160965856@qq.com>
2026-06-16 21:03:49 +0800

    6.16 add autopartol

--- chapt7/chapt7_ws/install/autopartol_robot/lib/python3.10/site-packages/autopartol_robot/partol_node.py
+++ chapt7/chapt7_ws/install/autopartol_robot/lib/python3.10/site-packages/autopartol_robot/partol_node.py
@@ -6,18 +6,41 @@ from rclpy.time import Time
 from tf2_ros import TransformListener, Buffer #坐标监听器
 from tf_transformations import euler_from_quaternion, quaternion_from_euler #四元数转欧拉角函数
 import math #角度转弧度函数
+from autopartol_interfaces.srv import SpeechText
+from sensor_msgs.msg import Image #消息接口
+from cv_bridge import CvBridge #转换图像格式
+import cv2 #保存图像
 
 
-class PartolNode(BasicNavigator):
+class PartolNode(BasicNavigator): 
     def __init__(self, node_name='partol_node'):
         super().__init__(node_name)
         # 声明相关参数
         self.declare_parameter('initial_point',[0.0, 0.0, 0.0])
         self.declare_parameter('target_points',[0.0, 0.0, 0.0, 1.0, 1.0, 1.57])
+        self.declare_parameter('img_save_path','')
+
         self.initial_point_ = self.get_parameter('initial_point').value
         self.target_points_ = self.get_parameter('target_points').value
+        self.img_save_path_ = self.get_parameter('img_save_path').value
+
         self.buffer_ = Buffer()
         self.listener_ = TransformListener(self.buffer_,self)
+        self.speech_client_ = self.create_client(SpeechText, 'speech_text')
+        self.cv_bridge_ = CvBridge()
+        self.latest_img_ = None
+        self.img_sub_ = self.create_subscription(Image, '/camera_sensor/image_raw', self.img_callback, 1)
+
+    def img_callback(self, msg):
+        self.latest_img_ = msg
+
+    def record_img(self):
+        if self.latest_img_ is not None:
+            pose = self.get_current_pose()
+            cv_image = self.cv_bridge_.imgmsg_to_cv2(self.latest_img_)
+            path = f'{self.img_save_path_}img_{pose.translation.x:3.2f}_{pose.translation.y:3.2f}.png'
+            cv2.imwrite(path,cv_image)
+
 
     def get_pose_by_xyyaw(self, x, y, yaw):
         """
@@ -84,17 +107,44 @@ class PartolNode(BasicNavigator):
             except Exception as e:
                 self.get_logger().warn(f'查询坐标变换失败:原因{str(e)}')
 
+    def speech_text(self, text):
+        """
+        调用服务合成语音
+        """
+        while not self.speech_client_.wait_for_service():
+            self.get_logger().info('语音合成服务未上线，等待中...')
+
+        request = SpeechText.Request()
+        request.text = text
+        future = self.speech_client_.call_async(request)
+        rclpy.spin_until_future_complete(self,future)
+        if future.result() is not None:
+            response = future.result()
+            if response.result == True:
+                self.get_logger().info(f'语音合成成功: {text}')
+            else:
+                self.get_logger().warn(f'语音合成失败: {text}')
+        else:
+            self.get_logger().warn(f'语音合成服务响应失败: {text}')
+
+
 def main():
     rclpy.init()
     partol = PartolNode() #节点
+    partol.speech_text('正在准备初始化位置')
     partol.init_robot_pose()
+    partol.speech_text('初始化位置完成')
 
     while rclpy.ok():
         points = partol.get_target_points()
         for point in points:
             x,y,yaw = point[0], point[1], point[2]
             target_pose = partol.get_pose_by_xyyaw(x, y, yaw)
+            partol.speech_text(f'正在准备前往{x},{y}目标点')
             partol.nav_to_pose(target_pose)
+            partol.speech_text(f'已经到达目标点{x},{y},正在准备记录图像')
+            partol.record_img()
+            partol.speech_text(f'图像记录完成')
 
     rclpy.shutdown()
 
--- chapt7/chapt7_ws/src/autopartol_robot/setup.py
+++ chapt7/chapt7_ws/src/autopartol_robot/setup.py
@@ -1,4 +1,5 @@
 from setuptools import find_packages, setup
+from glob import glob
 
 package_name = 'autopartol_robot'
 
@@ -10,6 +11,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name+"/launch", glob('launch/*.launch.py')),
         ('share/' + package_name+"/config", ['config/partol_config.yaml']),
     ],
     install_requires=['setuptools'],
@@ -25,7 +27,8 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'partol_node = autopartol_robot.partol_node:main'
+            'partol_node = autopartol_robot.partol_node:main',
+            'speaker = autopartol_robot.speaker:main',
         ],
     },
 )
