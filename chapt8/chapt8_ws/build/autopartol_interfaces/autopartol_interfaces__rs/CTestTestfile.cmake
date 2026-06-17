3923dfe218e74adb1e3c3930217a7b71d2b6cd23
loudeyi <1160965856@qq.com>
2026-06-17 15:05:30 +0800

    6.17 add global planner

---
+++ chapt8/chapt8_ws/build/autopartol_interfaces/autopartol_interfaces__rs/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/hzh/ros2_learn/chapt8/chapt8_ws/build/autopartol_interfaces/autopartol_interfaces__rs
+# Build directory: /home/hzh/ros2_learn/chapt8/chapt8_ws/build/autopartol_interfaces/autopartol_interfaces__rs
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ chapt8/chapt8_ws/build/autopartol_robot/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/hzh/ros2_learn/chapt8/chapt8_ws/install/autopartol_robot'
